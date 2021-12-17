e5eb24e413f58f7add33579f3670fcf1daec639b
Eden Baui <eedenbaui@gmail.com>
2021-12-17 16:50:29 +0800

    added routes, set up client, and config server

--- server/modules/itemSchema.js
+++ server/modules/itemSchema.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = mongoose.Schema({
   title: String,
-  due: Number,
   priority: String,
   details: String,
 });
--- server/routes/list.js
+++ server/routes/list.js
@@ -7,9 +7,24 @@ mongoose.connect(
   "mongodb+srv://admin-deb:test1234@cluster0.gw36x.mongodb.net/todolistDB?retryWrites=true&w=majority"
 );
 
+//get models
+const Item = require("../modules/item");
+const TaskList = require("../modules/taskList");
+
 //get list
-router.get("/", (req, res) => {
-  res.send("test");
+router.get("/", async (req, res) => {
+  const listCollection = await TaskList.find({});
+  res.send(listCollection);
+});
+
+//create list
+router.post("/", async (req, res) => {
+  const newTaskList = new TaskList({
+    name: req.body.name,
+    items: req.body.items,
+  });
+  await newTaskList.save();
+  res.status(201).send();
 });
 
 module.exports = router;
