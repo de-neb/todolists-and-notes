900bc3a2376319984d21a15768b4a2a5759c0a7a
Eden Baui <eedenbaui@gmail.com>
2022-01-19 23:51:08 +0800

    create signup and login page, remove outputpath in vue config

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 module.exports = {
   devServer: {
-    outputDir: path.resolve(__dirname, "../server/public"),
     proxy: {
       "/api": {
         target: "http://localhost:5000",
