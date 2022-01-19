afd70a7bdb007a25ed10d58023e6505d6bce0573
Eden Baui <eedenbaui@gmail.com>
2022-01-19 21:40:55 +0800

    prep vue config js for production

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   devServer: {
+    outputDir: path.resolve(__dirname, "../server/public"),
     proxy: {
       "/api": {
         target: "http://localhost:5000",
